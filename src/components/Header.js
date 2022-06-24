f2fd6d853e51398b66a069f90473f137d48c6d2e
Andrea <90292543+amgallo1213@users.noreply.github.com>
2022-06-23 22:33:34 -0400

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+    return ( 
+        <div className="Header">
+            <div className="Left">IMAGE of SCHOOL LOGO!</div>
+            <div className="Right">
+                <p>1-800-345-6789</p>
+                <p>Request Info</p>
+                <p>Login</p>
+                <button>Apply Now</button>
+            </div>
+        </div>
+     );
+}
+ 
+export default Header;
